a777ab147117a3f2461360e62e232e262f892966
GiuliaSossai <giulia.sossai@yahoo.it>
2021-11-02 14:01:22 +0100

    creo i quadratini e al click colo sfondo

--- js/script.js
+++ js/script.js
@@ -6,3 +6,41 @@ con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
 Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
  */
+
+const container = document.querySelector('.container');
+
+const listNumbers = [];
+init ();
+
+function init(){
+  for (let i = 0; i < 100; i++){
+    //creo ogni volta l'elemento square e lo aggiugno al container - con funzione
+  
+    const sq = createSquare(container);
+  
+    //gestisco comportamento - aggiungo colore sfondo al click
+    sq.addEventListener('click',function(){
+      console.log(this);
+      this.classList.add('clicked');
+  
+    });
+  }
+
+};
+
+/**
+ * generatore di square
+ * @param {HTMLDivElement} target 
+ * @returns 
+ */
+ function createSquare(target){
+   //creo elemento div
+  const sq = document.createElement('div');
+  //aggiungo classi a div
+  const classes = ['square', 'easy' ]
+  sq.classList.add(...classes);
+  //appendo il div al suo contenitore
+  target.append(sq);
+  //ottengo il quadrato
+  return sq;
+}
